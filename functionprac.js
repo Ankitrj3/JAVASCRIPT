873c50cfddd8d8210340b41e91477bda89c76012
Ankitrj3 <ankitrobinranjan@gmail.com>
2024-03-09 14:52:50 +0530

    function practice in javascript

---
+++ functionprac.js
@@ -0,0 +1,9 @@
+function arithmaticoperation( a, b){
+    console.log("addition of number = ",a+b);
+    console.log("sub of number = ",a-b);
+    console.log("multiply of number = "+a*b);
+    console.log("division of numbers = ",a/b);
+    console.log("modulas of number = ",a%b);
+}
+
+arithmaticoperation(122,34)
